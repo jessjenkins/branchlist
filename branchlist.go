c5d13683be811189a16d91c4b55a1794eee808a2
Jessica Jenkins <missjessjenkins@gmail.com>
2016-02-24 22:05:10 +0000

    Printout url

--- branchlist.go
+++ branchlist.go
@@ -6,5 +6,10 @@ import "os"
 
 func main() {
 	apikey := os.Getenv("GITHUB_APIKEY")
+	org := os.Getenv("GITHUB_ORG")
+	fmt.Printf("Org is %s\n", org)
 	fmt.Printf("Key is %s\n", apikey)
+	reposurl := "https://api.github.com/orgs/%s/repos?type=all&per-page=100&access_token=%s"
+	url := fmt.Sprintf(reposurl, org, apikey)
+	fmt.Printf("Url=%s\n", url)
 }
