8b99be3bcf5c368902c01b98a3bf1e1ad4a612a2
Jessica Jenkins <missjessjenkins@gmail.com>
2016-02-24 22:05:27 +0000

    Make GET request

--- branchlist.go
+++ branchlist.go
@@ -3,13 +3,28 @@ package main
 import "fmt"
 import "os"
 
+import "net/http"
+import "io/ioutil"
+import "log"
 
 func main() {
 	apikey := os.Getenv("GITHUB_APIKEY")
 	org := os.Getenv("GITHUB_ORG")
 	fmt.Printf("Org is %s\n", org)
 	fmt.Printf("Key is %s\n", apikey)
+
 	reposurl := "https://api.github.com/orgs/%s/repos?type=all&per-page=100&access_token=%s"
 	url := fmt.Sprintf(reposurl, org, apikey)
 	fmt.Printf("Url=%s\n", url)
+
+	res, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	json, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", json)
 }
