bcafd16caedc7045b60b3d4133f8faa2a2e0925b
Jessica Jenkins <missjessjenkins@gmail.com>
2016-03-25 18:07:36 +0000

    Print out repos per branch name

--- branchlist.go
+++ branchlist.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/missjessjenkins/branchlist/config"
 	"github.com/missjessjenkins/branchlist/repos"
@@ -26,17 +27,30 @@ func main() {
 	}
 
 	// When each fired off routine above finishes, collect up
-	// the results and print them out
+	// the results and add to a map
+	branchmap := make(map[string][]string)
 	for i := 0; i < numrepos; i++ {
 		rbl := <-cr
-		fmt.Printf("%d - %s\n", i, rbl.Repo)
-		for x, br := range rbl.Branches {
-			fmt.Printf("  %d - %s\n", x, br)
+		//fmt.Printf("%d - %s\n", i, rbl.Repo)
+		for _, br := range rbl.Branches {
+			//fmt.Printf("  %d - %s\n", x, br)
+			branchmap[br] = append(branchmap[br], rbl.Repo)
 		}
 	}
 
+	// Sort and print out list of repos per branchname
+	branches := make([]string, 0, len(branchmap))
+	for branch := range branchmap {
+		branches = append(branches, branch)
+	}
+	sort.Strings(branches)
+
+	for _, branch := range branches {
+		fmt.Printf("%s: %v\n", branch, branchmap[branch])
+	}
 }
 
+// =============================================================
 type repoBranchList struct {
 	Repo     string
 	Branches []string
